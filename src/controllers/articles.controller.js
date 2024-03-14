9d79f977ab52d2cdaeab42ae2ff6b0fdee9df68d
seunggyu <devkim0702@gmail.com>
2024-03-14 13:09:17 +0900

    Feat: complete sendMail contoller to handle POST request to /articles/sendEmail API

--- src/controllers/articles.controller.js
+++ src/controllers/articles.controller.js
@@ -1,5 +1,6 @@
 const Article = require("../models/Article");
 const User = require("../models/User");
+const nodemailer = require("nodemailer");
 
 exports.saveArticle = async function (req, res, next) {
   const { user, articleContent, articleId } = req.body;
@@ -47,4 +48,100 @@ exports.getArticle = async function (req, res, next) {
   } catch (error) {
     console.log(error);
   }
+};
+
+exports.sendEmail = async function (req, res, next) {
+  console.log("send email request");
+  const { emailList, url, articleId } = req.body;
+
+  const article = await Article.findById(articleId);
+  const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
+    },
+  });
+
+  const prevReviewersList = [];
+
+  for (const prevReviewer of article.reviewers) {
+    prevReviewersList.push(prevReviewer.email);
+  }
+
+  const removedReviewersList = prevReviewersList.filter(
+    (el) => emailList.indexOf(el) === -1,
+  );
+
+  for (let i = 0; i < removedReviewersList.length; i++) {
+    const user = await User.findOne({ email: removedReviewersList[i] });
+
+    if (user) {
+      await User.findByIdAndUpdate(user._id, {
+        $pull: { reviewedArticles: articleId },
+      });
+    }
+  }
+
+  const newReviewersList = [];
+
+  for (let i = 0; i < emailList.length; i++) {
+    const reviewerObj = { email: emailList[i], status: "pending" };
+
+    newReviewersList.push(reviewerObj);
+  }
+
+  for (let i = 0; i < newReviewersList.length; i++) {
+    const user = await User.findOne({ email: newReviewersList[i].email });
+
+    if (user) {
+      newReviewersList[i].user = user._id;
+
+      await User.findByIdAndUpdate(user._id, {
+        $addToSet: { reviewedArticles: articleId },
+      });
+    }
+  }
+
+  article.reviewers = newReviewersList;
+
+  await article.save();
+
+  const sendEmailPromises = emailList.map((el) => {
+    return new Promise((resolve, reject) => {
+      const mailOptions = {
+        from: process.env.EMAIL,
+        to: el,
+        subject: "Review request",
+        text: `Please review my post. ${url}`,
+      };
+
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.log("Error sending email to " + email + ": ", error);
+          reject(error);
+        } else {
+          console.log("Email sent to " + email + ": " + info.response);
+          resolve(info.response);
+        }
+      });
+    });
+  });
+
+  Promise.all(sendEmailPromises)
+    .then((responses) => {
+      console.log("All emails sent successfully.");
+
+      res
+        .status(200)
+        .send({ result: "ok", message: "Emails sent successfully" });
+    })
+    .catch((error) => {
+      console.log("Error sending one or more emails: ", error);
+
+      res.status(500).send({ result: "error", message: error.message });
+    });
 };
