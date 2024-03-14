864c0671e54c69d0187f23bb2944e68449e996f8
seunggyu <devkim0702@gmail.com>
2024-03-14 13:10:32 +0900

    Feat: add routers for GET/articles/getArticle, POST/articles/saveArticle, and POST/articles/email API

--- src/routes/articles.js
+++ src/routes/articles.js
@@ -4,6 +4,9 @@ const articlesController = require("../controllers/articles.controller");
 
 const router = express.Router();
 
-router.get("/", articlesController.getArticles);
+router.get("/", articlesController.getArticle);
+router.post("/", articlesController.saveArticle);
+
+router.post("/email", articlesController.sendEmail);
 
 module.exports = router;
