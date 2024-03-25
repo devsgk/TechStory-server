077ad39dc5b195e97f0ec85c8d143e0d555d3a42
seunggyu <devkim0702@gmail.com>
2024-03-25 15:12:48 +0900

    Feat: create /users route

--- src/loaders/routers.js
+++ src/loaders/routers.js
@@ -1,9 +1,11 @@
 const articlesRouter = require("../routes/articles");
 const authRouter = require("../routes/auth");
+const usersRouter = require("../routes/users");
 
 async function routerLoader(app) {
   app.use("/auth", authRouter);
   app.use("/articles", articlesRouter);
+  app.use("/users", usersRouter);
 }
 
 module.exports = routerLoader;
